ea9bd1e147c4352ef426c1542e3c986d29cf4809
jaren <jarensmart@yahoo.com>
2020-02-15 04:50:42 -0500

    mouseover finished

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-// Your code goes here
-//init commit
+const logo = document.querySelector(".logo-heading");
+const navCon = document.querySelector(".container nav-container");
+
+//mouseover event
+logo.addEventListener("mouseover", () => {
+  logo.style.transform = "scale(1.3)";
+  logo.style.transition = "all 0.4s";
+  logo.style.cursor = "pointer";
+});
+
+logo.addEventListener("mouseleave", () => {
+  logo.style.transform = "scale(1)";
+});
+
+//keydown event (B - 66)
+navCon.addEventListener("keydown", function(e) {
+  if (event.keyCode == 66) {
+    navCon.style.background = "#FFEBCE";
+  }
+});
